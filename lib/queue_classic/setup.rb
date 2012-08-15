a23ba41bc0de70327a8d405f777c96803efa0af8
Matt Colyer <matt@colyer.name>
2012-08-15 15:25:53 -0700

    Add QC::Setup.create_job_count_index
    Creates an index so that we can determine if a job has been queued.

--- lib/queue_classic/setup.rb
+++ lib/queue_classic/setup.rb
@@ -3,11 +3,13 @@ module QC
     Root = File.expand_path("../..", File.dirname(__FILE__))
     SqlFunctions = File.join(Root, "/sql/ddl.sql")
     CreateTable = File.join(Root, "/sql/create_table.sql")
+    CreateJobCountIndex = File.join(Root, "/sql/create_job_count_index.sql")
     DropSqlFunctions = File.join(Root, "/sql/drop_ddl.sql")
 
     def self.create
       Conn.execute(File.read(CreateTable))
       Conn.execute(File.read(SqlFunctions))
+      Conn.execute(File.read(CreateJobCountIndex))
     end
 
     def self.drop
